93bc26d370b8228171da0740e67f3b50f536c46d
Janu Aditya <yanuart.adityan@gmail.com>
2015-11-13 14:42:29 +0100

    Added eigen3

---
+++ CMakeFiles/vse-kalman.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vse-kalman.dir/main/vseMain.cpp.o"
+  "vse-kalman.pdb"
+  "vse-kalman"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vse-kalman.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
